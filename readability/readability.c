9ff697efbf5515c0bcda6808e9ce41d20a2a0602
Aviral1344 <97392425+Aviral1344@users.noreply.github.com>
2023-08-12 15:02:58 +0000

    Sat, Aug 12, 2023, 8:32 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -1,7 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    string text = get_string("Text: ");
+
+    int words = 0;
+    int letters = 0;
+    int sentences = 0;
+
+    int len = strlen(text);
+
+    for(int i=0; i<len; i++){
+        if(text[i])
+    }
 
 }
