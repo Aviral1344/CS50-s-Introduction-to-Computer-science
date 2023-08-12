367311853eeb685ceca71d0e65b72a748f9c8c25
Aviral1344 <97392425+Aviral1344@users.noreply.github.com>
2023-08-12 15:10:36 +0000

    Sat, Aug 12, 2023, 8:40 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int count_letters(string text){
-    
+    int len = strlen(text);
+    int count = 0;
+    for(int i=0; i<len; i++){
+        if(text[i] >= 'a' && text[i] <= 'z' || text[i] >= 'A' && text[i] <= 'Z'){
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_words(string text){
+    int len = strlen(text);
+    int count = 0;
+    for(int i=0; i<len; i++){
+        if(text[i] == ' ' && (text[i-1] == '.' || text[i-1] == '!')){
+            continue;
+        }
+        if(text[i] == '.' || text[i] == '!' || text[i] == ' '){
+            count++;
+        }
+    }
+    return count;
 }
 
 int main(void)
