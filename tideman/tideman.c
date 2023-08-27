10a0ebafdcfc8fc3871c9f105e763c097046fb96
Aviral1344 <97392425+Aviral1344@users.noreply.github.com>
2023-08-27 21:39:20 +0000

    Mon, Aug 28, 2023, 3:09 AM +05:30

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -127,7 +127,11 @@ void add_pairs(void)
     // TODO
     for(int i=0; i<candidate_count; i++){
         for(int j=0; j<candidate_count; j++){
-            
+            if(preferences[i][j] > preferences[j][i]){
+                pair new_pair = {i,j};
+                pairs[pair_count] = new_pair;
+                pair_count += 1;
+            }
         }
     }
     return;
