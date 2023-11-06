34238e75bc89258aa2598e47c971ac593b6d1205
Aviral1344 <97392425+Aviral1344@users.noreply.github.com>
2023-11-06 16:16:16 +0000

    Mon, Nov 6, 2023, 9:46 PM +05:30

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,5 +1,12 @@
 # TODO
 
-while true:
-    try:
-        
+def main():
+    credit_card = get_num()
+
+def get_num():
+    while True:
+        num = int(input("Number: "))
+        try:
+            if len(card_num) > 0 and num:
+                break
+
