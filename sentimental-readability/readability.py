64a7baea7dec990d9b22a3cd380798815115d7f2
Aviral1344 <97392425+Aviral1344@users.noreply.github.com>
2023-12-20 09:57:23 +0000

    Wed, Dec 20, 2023, 3:27 PM +05:30

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -11,8 +11,8 @@ letters = 0
 words = 0
 sentences = 0
 
-# Loop over setence list
 for word in sentence:
+
     # Count words
     words += 1
 
@@ -24,10 +24,6 @@ for word in sentence:
     sentence_delims = ['!', '?', '.']
     if any(delim in word for delim in sentence_delims):
         sentences += 1
-    # Above is the same as:
-    # for delim in sentence_delims:
-    #     if delim in word:
-
 
 AVERAGE = 100 / words
 # Coleman-Liau Index
