8786766059240a56f0a2a02afc5c58f00997619c
Aviral1344 <97392425+Aviral1344@users.noreply.github.com>
2023-12-31 07:57:24 +0000

    Sun, Dec 31, 2023, 1:27 PM +05:30

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,27 +1,30 @@
 
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    printf("Size of your pyramid: ");
-    int size = get_int();
-    while (size < 0 || size >= 24){
-        printf("Please choose a size between 0 and 23: ");
-        size = get_int();
+    // Declare int here globaly to be used in loops further down:
+    int height;
+    // Do while loop to get int between 1 and including 8:
+    do
+    {
+        height = get_int("Height: ");
     }
-
-    for (int i = 1; i < size + 1; i++){
-        for (int j = 0; j < size + 1; j++){
-
-            if (j < size - i){
-                printf(" ");
-            }
-            else{
-                printf("#");
-            }
+    while (height < 1 || height > 8);
+    // Parent for loop for number of rows that will equal the height input:
+    for (int rows = 0; rows < height; rows++)
+    {
+        // Child for loop, to print spaces:
+        for (int spaces = height - 1; spaces > rows; spaces--)
+        {
+            printf(" ");
+        }
+        // Child for loop, to print hashes:
+        for (int hashes = 0; hashes <= rows; hashes++)
+        {
+            printf("#");
         }
         printf("\n");
     }
-
 }
