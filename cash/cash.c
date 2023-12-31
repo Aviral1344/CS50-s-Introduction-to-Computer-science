8cb9ff8a9c3045062f2fb0586de3a52baa128340
Aviral1344 <97392425+Aviral1344@users.noreply.github.com>
2023-12-31 08:08:44 +0000

    Sun, Dec 31, 2023, 1:38 PM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -1,65 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef uint8_t BYTE;
 
+// Digital camera FAT file block size: 512 Bytes
+const int BLOCK_SIZE = 512;
+
+bool has_JPEG_header(BYTE *buffer);
+
 int main(int argc, char *argv[])
 {
-    // Check usage
+    // Ensure proper usage
     if (argc != 2)
     {
-        printf("Usage: ./recover image\n");
+        printf("Usage: ./recover IMAGE\n");
         return 1;
     }
 
-    // Open file
-    FILE *file = fopen(argv[1], "r");
-    if (!file)
+    FILE *inptr = fopen(argv[1], "rb");
+    if (inptr == NULL)
     {
+        printf("Could not read %s.\n", argv[1]);
         return 1;
     }
 
-    FILE *img = NULL;
-
-    BYTE buffer[512];
-    char filename[8];
-    int counter = 0;
-    while (fread(buffer, sizeof(BYTE), 512, file) == 512)
+    BYTE *buffer = malloc(BLOCK_SIZE * sizeof (BYTE));
+    if (buffer == NULL)
     {
-        // checks if start of img in buffer
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
-        {
-            // if start of img and first image i.e. counter ==0
-            // then begins writing a new image
-            if (counter == 0)
-            {
-                sprintf(filename, "%03i.jpg", counter);
-                img = fopen(filename, "w");
-                fwrite(&buffer, sizeof(BYTE), 512, img);
-                counter += 1;
-            }
-            // if start of img but not first image then
-            // closes the image and begins writing new image
-            else if (counter > 0)
-            {
-                fclose(img);
-                sprintf(filename, "%03i.jpg", counter);
-                img = fopen(filename, "w");
-                fwrite(&buffer, sizeof(BYTE), 512, img);
-                counter += 1;
-            }
-        }
-        // if not start of new img
-        // then it keeps on writing the image
-        else if (counter > 0)
-        {
-            fwrite(&buffer, sizeof(BYTE), 512, img);
-        }
-
+        printf("Could not allocate memory.\n");
+        return 1;
     }
 
-    // Close file
-    fclose(file);
-    fclose(img);
+    FILE *outptr = NULL;
+    int img_count = 0;
+
+    // Recover JPEGs from input file
+    while (fread(buffer, 1, BLOCK_SIZE, inptr) == BLOCK_SIZE)
+    {
+        if (has_JPEG_header(buffer))
+        {
+            // Close previous file, if it exists
+            if(outptr != NULL)
+            {
+                fclose(outptr);
+            }
+
+            // Create new file
+            char filename[8]; // ###.jpg\0
+            sprintf(filename, "%03i.jpg", img_count);
+
+            outptr = fopen(filename, "wb");
+            if (outptr == NULL)
+            {
+                printf("Could not create %s.\n", filename);
+                fclose(inptr);
+                free(buffer);
+                return 1;
+            }
+            img_count++;
+        }
+
+        if (outptr != NULL)
+        {
+            fwrite(buffer, 1, BLOCK_SIZE, outptr);
+        }
+    }
+
+    free(buffer);
+    fclose(inptr);
+    fclose(outptr);
+    return 0;
+}
+
+// Check if the first 4 bytes of the buffer are the JPEG header
+bool has_JPEG_header(BYTE *buffer)
+{
+    if (buffer[0] == 0xff &&
+        buffer[1] == 0xd8 &&
+        buffer[2] == 0xff &&
+        (buffer[3] & 0xE0) == 0xE0)
+    {
+        return true;
+    }
+    return false;
 }
