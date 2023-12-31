0c36b53245b8ac5906a4f91e14964e5324de6def
Aviral1344 <97392425+Aviral1344@users.noreply.github.com>
2023-12-31 08:13:29 +0000

    Sun, Dec 31, 2023, 1:43 PM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -1,88 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <stdbool.h>
 
-typedef uint8_t BYTE;
-
-// Digital camera FAT file block size: 512 Bytes
 const int BLOCK_SIZE = 512;
-
-bool has_JPEG_header(BYTE *buffer);
 
 int main(int argc, char *argv[])
 {
-    // Ensure proper usage
+    // Check for proper usage
     if (argc != 2)
     {
-        printf("Usage: ./recover IMAGE\n");
+        printf("Usage: ./recover filename.raw\n");
         return 1;
     }
 
-    FILE *inptr = fopen(argv[1], "rb");
-    if (inptr == NULL)
+    // Open memory card
+    FILE *file = fopen(argv[1], "r");
+
+    if (file == NULL)
     {
-        printf("Could not read %s.\n", argv[1]);
+        printf("Could not open %s.\n", argv[1]);
         return 1;
     }
 
-    BYTE *buffer = malloc(BLOCK_SIZE * sizeof (BYTE));
-    if (buffer == NULL)
-    {
-        printf("Could not allocate memory.\n");
-        return 1;
-    }
+    // Create buffer - pointer to where to store the read data
+    typedef uint8_t BYTE;
+    BYTE buffer[BLOCK_SIZE];
 
-    FILE *outptr = NULL;
-    int img_count = 0;
+    // Counter for number of JPEGs found on the memory card
+    int counter = 0;
 
-    // Recover JPEGs from input file
-    while (fread(buffer, 1, BLOCK_SIZE, inptr) == BLOCK_SIZE)
+    // Allocate enough bytes for the name of the file
+    char *filename = malloc(8 * sizeof(char));
+
+    // Output file
+    FILE *output = NULL;
+
+    // Loop through the data on the card and read every block of 512 bytes into a buffer
+    while (fread(buffer, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
     {
-        if (has_JPEG_header(buffer))
+        // Check if it's the start of a new JPEG
+        if (buffer[0] == 0xFF && buffer[1] == 0xD8 && buffer[2] == 0xFF && (buffer[3] & 0xF0) == 0xE0)
         {
-            // Close previous file, if it exists
-            if(outptr != NULL)
+            // If it's the first JPEG
+            if (counter == 0)
             {
-                fclose(outptr);
+                // Start writing the first file - 000.jpg
+                sprintf(filename, "%03i.jpg", counter);
+                output = fopen(filename, "w");
+                fwrite(buffer, 1, BLOCK_SIZE, output);
+
+                counter++;
             }
 
-            // Create new file
-            char filename[8]; // ###.jpg\0
-            sprintf(filename, "%03i.jpg", img_count);
-
-            outptr = fopen(filename, "wb");
-            if (outptr == NULL)
+            // If already found a JPEG
+            else if (counter > 0)
             {
-                printf("Could not create %s.\n", filename);
-                fclose(inptr);
-                free(buffer);
-                return 1;
+                // Close the current file and open up a new file to continue writing to
+                fclose(output);
+                sprintf(filename, "%03i.jpg", counter);
+                output = fopen(filename, "w");
+                fwrite(buffer, 1, BLOCK_SIZE, output);
+                counter++;
             }
-            img_count++;
         }
 
-        if (outptr != NULL)
+        // If it's not the start of a new JPEG
+        else if (counter > 0)
         {
-            fwrite(buffer, 1, BLOCK_SIZE, outptr);
+            // Keep writing to it because it's the next 512 bytes block of the current JPEG
+            fwrite(buffer, 1, BLOCK_SIZE, output);
         }
     }
 
-    free(buffer);
-    fclose(inptr);
-    fclose(outptr);
-    return 0;
-}
-
-// Check if the first 4 bytes of the buffer are the JPEG header
-bool has_JPEG_header(BYTE *buffer)
-{
-    if (buffer[0] == 0xff &&
-        buffer[1] == 0xd8 &&
-        buffer[2] == 0xff &&
-        (buffer[3] & 0xE0) == 0xE0)
-    {
-        return true;
-    }
-    return false;
+    // Close remaining files
+    free(filename);
+    fclose(file);
+    fclose(output);
 }
